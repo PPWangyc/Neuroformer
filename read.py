6c8330dbdb886ddb7df4445628d690624f96d877
PPWangyc <ppwangyc@gmail.com>
2024-09-17 21:11:05 -0700

    add

--- read.py
+++ read.py
@@ -18,12 +18,53 @@ print(len(data['ID']))
 print(len(data['time']))
 print(len(data['dt'])) 
 print(len(data['Trial']))
+print(len(data['true_dt']))
 
-config, tokenizer, model = load_model_and_tokenizer(folder)
-print(tokenizer)
-decoded_data = tokenizer.decode(data['ID'], 'ID')
-print(len(decoded_data))
-print(decoded_data)
-decoded_dt = tokenizer.decode(data['dt'], 'dt')
-decoded_trial = tokenizer.decode(data['Trial'], 'Trial')
-# decoded_time = tokenizer.decode(data['time'], 'time')
+gt_id = data['true']
+# gt_id is a list of tensor, convert it to numpy array
+gt = []
+for i in range(len(gt_id)):
+    gt.append(gt_id[i].cpu())
+neuron_ids = np.unique(gt)
+neuron_ids = np.unique(data['ID'])
+time_points = np.unique(data['dt'])
+trials = np.unique(data['Trial'])
+print(len(neuron_ids))
+print(time_points)
+print(trials)
+exit()
+total_neurons = 185
+# Initialize a matrix of zeros with dimensions [number of neurons X number of time points]
+spiking_matrix = np.zeros((total_neurons, len(time_points)))
+
+# Create a mapping from neuron ID and time to matrix indices for quick lookup
+# neuron_index = {neuron_id: idx for idx, neuron_id in enumerate(neuron_ids)}
+# print(neuron_index)
+# exit()
+time_index = {time: idx for idx, time in enumerate(time_points)}
+
+time2idx = {time: idx for idx, time in enumerate(time_points)}
+print(data['Trial'])
+# Populate the matrix
+for idx, neuron_id in enumerate(data['ID']):
+    neuron_idx = neuron_id
+    time_idx = time_index[str(data['time'][idx])]
+    spiking_matrix[neuron_idx, time_idx] += 1  # Assuming each entry in 'ID' represents a single spike
+
+import matplotlib.pyplot as plt
+
+plt.imshow(spiking_matrix, aspect='auto', interpolation='none')
+plt.colorbar()
+plt.xlabel('Time Points')
+plt.ylabel('Neurons')
+plt.title('Spiking Activity Matrix')
+plt.savefig('spiking_activity.png')
+print("Spiking activity matrix saved as 'spiking_activity.png'")
+# config, tokenizer, model = load_model_and_tokenizer(folder)
+# print(tokenizer)
+# decoded_data = tokenizer.decode(data['ID'], 'ID')
+# print(len(decoded_data))
+# print(decoded_data)
+# decoded_dt = tokenizer.decode(data['dt'], 'dt')
+# decoded_trial = tokenizer.decode(data['Trial'], 'Trial')
+# # decoded_time = tokenizer.decode(data['time'], 'time')
