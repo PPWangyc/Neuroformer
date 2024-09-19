46779d34a7f8aa3748ac1939b878276371580dbb
PPWangyc <ppwangyc@gmail.com>
2024-09-18 23:07:21 -0700

    add

--- script/default.sh
+++ script/default.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+#SBATCH --account=col169
+#SBATCH --partition=gpu-shared
+#SBATCH --job-name="mm"
+#SBATCH --output="mm.%j.out"
+#SBATCH -N 1
+#SBACTH --array=0
+#SBATCH -c 8
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem 150000
+#SBATCH --gpus=1
+#SBATCH -t 2-00
+#SBATCH --export=ALL
+module load gpu
+module load slurm
+. ~/.bashrc
+
 conda activate neuroformer
 cd ..
 python neuroformer_train.py \
